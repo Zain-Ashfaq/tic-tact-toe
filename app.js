8f06be74926862402a551d7c3374c75b2da9d8e0
Zain-Ashfaq <zashfaq00@gmail.com>
2022-10-05 15:12:47 +0100

    Added psuedo code to plan out game

---
+++ app.js
@@ -0,0 +1,38 @@
+/*
+
+plan out tic tac toe game
+
+Have a game board. 3x3 css grid.
+
+Encase everything in a while loop. Will return false once player 1 or player 2 has won.
+
+have a bool checker to see whos turn it is. Player 1 starts out false. Has to be a global var.
+
+Player 1 will be X - Player 2 will be O
+
+Original grid will be blank squares. Once a player clicks on a square, either an X or O class will be added over the blank square. This needs a bool checker. Starts out false. Once a player clicks on blank square, it turns true and exits out the curly brackets.
+
+Each blank square has to have individual button 
+
+////
+Need to find out how to manipulate specific area of css grid
+////
+
+needs winning set arrays to check against game board. Board will be 9 squares e.g need 1,4,7 = win 7,5,3. These values ARENT 0 index. For psuedo use 1-9 
+
+could use empty array that stores X and O values. Loop through array. indexes could be hardcoded as game only has a potential number of ways to win e.g 1,4,7 works but 4,2,3 doesnt.
+
+
+have button that fully resets everything. Just set all global var to 0. 
+
+have semi reset button. Clears board but keeps score. would be global var to keep score.
+
+
+
+
+
+
+
+
+
+*/
